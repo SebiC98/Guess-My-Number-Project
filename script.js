6a76f78f826d70a8e04663af73a634ba7a33f2e0
SebiC98 <35177820+SebiC98@users.noreply.github.com>
2021-01-05 18:23:25 +0200

    Implementing the Game Logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too small!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  }
+});
